9d0ded6664613633d128efcbba100060bc23fff0
Sid Mann <sjmann2@outlook.com>
2022-08-02 11:46:34 -0500

    add tests for pantry iteration 3

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -1,30 +1,58 @@
-require './lib/pantry'
-require './lib/ingredient'
+require "./lib/pantry"
+require "./lib/ingredient"
 
 describe Pantry do
-  before :each do
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-    @pantry = Pantry.new
+  describe "Iteration 1" do
+    before :each do
+      @ingredient1 = Ingredient.new({ name: "Cheese", unit: "oz", calories: 50 })
+      @ingredient2 = Ingredient.new({ name: "Macaroni", unit: "oz", calories: 200 })
+      @pantry = Pantry.new
+    end
+
+    it "exists" do
+      expect(@pantry).to be_a(Pantry)
+    end
+
+    it "has no stock to start" do
+      expect(@pantry.stock).to eq({})
+      expect(@pantry.stock_check(@ingredient1)).to eq(0)
+    end
+
+    it "can restock ingredients" do
+      @pantry.restock(@ingredient1, 5)
+      @pantry.restock(@ingredient1, 10)
+
+      expect(@pantry.stock_check(@ingredient1)).to eq(15)
+
+      @pantry.restock(@ingredient2, 7)
+
+      expect(@pantry.stock_check(@ingredient2)).to eq(7)
+    end
   end
 
-  it 'exists' do
-    expect(@pantry).to be_a(Pantry)
-  end
+  describe "Iteration 3" do
+    before :each do
+      pantry = Pantry.new
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      @recipe1 = Recipe.new("Mac and Cheese")
+      @recipe2 = Recipe.new("Cheese Burger")
+    end
 
-  it 'has no stock to start' do
-    expect(@pantry.stock).to eq({})
-    expect(@pantry.stock_check(@ingredient1)).to eq(0)
-  end
-
-  it 'can restock ingredients' do
+    it 'can tell if it has enough incredients for a recipe' do
     @pantry.restock(@ingredient1, 5)
     @pantry.restock(@ingredient1, 10)
 
-    expect(@pantry.stock_check(@ingredient1)).to eq(15)
+    expect(@pantry.enough_ingredients_for?(@recipe1)).to be(false)
 
-    @pantry.restock(@ingredient2, 7)
+    pantry.restock(ingredient2, 7)
 
-    expect(@pantry.stock_check(@ingredient2)).to eq(7)
+    expect(@pantry.enough_ingredients_for?(@recipe1)).to be(false)
+
+    @pantry.restock(@ingredient2, 1)
+
+    expect(@pantry.enough_ingredients_for?(@recipe1)).to eq(true)
   end
 end
