0ad92bc186313144736ff992627fc6822560e896
Sid Mann <sjmann2@outlook.com>
2022-08-02 11:41:21 -0500

    progress on highest calorie meal

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -19,5 +19,13 @@ class CookBook
     ingredients.uniq
   end
 
-  
-end
+  # COME BACK TO THIS
+  def highest_calorie_meal
+    recipe_calories =
+    @recipes.flat_map do |recipe|
+      [recipe, recipe.total_calories]
+      #recipe object, total calories
+    end 
+    recipe_calories.max { |meal_1, meal_2| meal_1[1] <=> meal_2[1] }
+    end
+  end
