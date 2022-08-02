4fafdad65f2b0f3e94f4df45067a7e78542d2f19
Sid Mann <sjmann2@outlook.com>
2022-08-02 11:12:34 -0500

    add ingredients to cookbook class

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,16 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredients = []
+    @recipes.each do |recipe|
+      recipe.ingredients.each do |ingredient|
+        ingredients << ingredient.name
+      end
+    end
+    ingredients.uniq
+  end
+
+  
 end
