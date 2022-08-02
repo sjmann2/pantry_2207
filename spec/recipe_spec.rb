722bac8bd6eed4c6e7f4663f60d2add13cac9a2f
Sid Mann <sjmann2@outlook.com>
2022-08-02 10:42:46 -0500

    add tests for iteration 3

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,29 +1,53 @@
-require './lib/ingredient'
-require './lib/recipe'
+require "./lib/ingredient"
+require "./lib/recipe"
 
 describe Recipe do
-  before :each do
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-    @recipe1 = Recipe.new("Mac and Cheese")
+  describe "Iteration 1 and 2" do
+    before :each do
+      @ingredient1 = Ingredient.new({ name: "Cheese", unit: "C", calories: 100 })
+      @ingredient2 = Ingredient.new({ name: "Macaroni", unit: "oz", calories: 30 })
+      @recipe1 = Recipe.new("Mac and Cheese")
+    end
+
+    it "exists" do
+      expect(@recipe1).to be_a(Recipe)
+    end
+
+    it "has a name" do
+      expect(@recipe1.name).to eq("Mac and Cheese")
+    end
+
+    it "can add ingredients required" do
+      expect(@recipe1.ingredients_required).to eq({})
+
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+
+      expect(@recipe1.ingredients_required).to eq({ @ingredient1 => 6, @ingredient2 => 8 })
+      expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+    end
   end
 
-  it 'exists' do
-    expect(@recipe1).to be_a(Recipe)
-  end
+  describe "Iteration 3" do
+    before :each do
+      @pantry = Pantry.new
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      @recipe1 = Recipe.new("Mac and Cheese")
+      @recipe2 = Recipe.new("Cheese Burger")
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
+    end
 
-  it 'has a name' do
-    expect(@recipe1.name).to eq("Mac and Cheese")
-  end
-
-  it 'can add ingredients required' do
-    expect(@recipe1.ingredients_required).to eq({})
-
-    @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient1, 4)
-    @recipe1.add_ingredient(@ingredient2, 8)
-
-    expect(@recipe1.ingredients_required).to eq({@ingredient1 => 6, @ingredient2 => 8})
-    expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+    it 'can calculate total calories' do
+      expect(@recipe1.total_calories).to eq(440)
+      expect(@recipe2.total_calories).to eq(675)
+    end
   end
 end
