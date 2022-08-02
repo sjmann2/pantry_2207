2f5b3f9e85a8452d080adfa68828ff6d03f6d24e
Sid Mann <sjmann2@outlook.com>
2022-08-02 12:36:11 -0500

    progress on enough ingrdients for

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -12,4 +12,17 @@ class Pantry
   def restock(ingredient, count)
     @stock[ingredient] += count
   end
+
+  def enough_ingredients_for?(recipe)
+    #check that ingredients required matches
+    #the stock ingredients in the correct quantity
+    enough_ingredients_for =
+      recipe.ingredients_required.flat_map do |recipe_ingredient, recipe_count|
+        @stock.map do |stock_ingredient, stock_count|
+          stock_ingredient == recipe_ingredient && stock_count >= recipe_count
+     
+      end
+    end
+    # enough_ingredients_for.all?(true)
+  end
 end
--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -1,5 +1,6 @@
 require "./lib/pantry"
 require "./lib/ingredient"
+require "./lib/recipe"
 
 describe Pantry do
   describe "Iteration 1" do
@@ -32,27 +33,32 @@ describe Pantry do
 
   describe "Iteration 3" do
     before :each do
-      pantry = Pantry.new
-      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
-      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
-      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      @pantry = Pantry.new
+      @ingredient1 = Ingredient.new({ name: "Cheese", unit: "C", calories: 100 })
+      @ingredient2 = Ingredient.new({ name: "Macaroni", unit: "oz", calories: 30 })
+      @ingredient3 = Ingredient.new({ name: "Ground Beef", unit: "oz", calories: 100 })
+      @ingredient4 = Ingredient.new({ name: "Bun", unit: "g", calories: 75 })
       @recipe1 = Recipe.new("Mac and Cheese")
       @recipe2 = Recipe.new("Cheese Burger")
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
     end
 
-    it 'can tell if it has enough incredients for a recipe' do
-    @pantry.restock(@ingredient1, 5)
-    @pantry.restock(@ingredient1, 10)
+    it "can tell if it has enough incredients for a recipe" do
+      @pantry.restock(@ingredient1, 5)
+      @pantry.restock(@ingredient1, 10)
 
-    expect(@pantry.enough_ingredients_for?(@recipe1)).to be(false)
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to be(false)
 
-    pantry.restock(ingredient2, 7)
+      @pantry.restock(@ingredient2, 7)
 
-    expect(@pantry.enough_ingredients_for?(@recipe1)).to be(false)
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to be(false)
 
-    @pantry.restock(@ingredient2, 1)
-
-    expect(@pantry.enough_ingredients_for?(@recipe1)).to eq(true)
+      @pantry.restock(@ingredient2, 1)
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to eq(true)
+    end
   end
 end
